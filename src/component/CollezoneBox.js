13882c76748cd70a0a26d9ae951d44f228433eb1
Anurag Patel <anuragpatel1281@gmail.com>
2021-06-15 19:04:30 +0530

    modal

--- src/component/CollezoneBox.js
+++ src/component/CollezoneBox.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "../css/CollezoneBox.css"
 import { Avatar } from "@material-ui/core"
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/userSlice'
 function CollezoneBox() {
+  const user = useSelector(selectUser)
   return (
     <div className="collezoneBox">
        <div className="collezoneBox_info">
-       <Avatar/>
-        <h5>Username</h5>
+       <Avatar
+         src ={user.photo}
+       />
+        <h5>{user.displayName}</h5>
        </div>
        <div className="collezoneBox_collezone">
         <p>What is your question or link?</p>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeIcon from '@material-ui/icons/Home';
 import FeaturedPlayListOutlinedIcon from '@material-ui/icons/FeaturedPlayListOutlined';
 import AssignmentTurnedInOutlinedIcon from '@material-ui/icons/AssignmentTurnedInOutlined';
@@ -6,12 +6,16 @@ import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
 import NotificationOutlinedIcon from '@material-ui/icons/NotificationsOutlined';
 import SearchIcon from '@material-ui/icons/Search';
 import LanguageIcon from '@material-ui/icons/Language';
-import { Avatar, Button } from '@material-ui/core';
+import { Avatar, Button, Input } from '@material-ui/core';
 import '../css/Navbar.css';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
-import  {auth} from "../firebase";
+import  db, {auth} from "../firebase";
 import { useEffect } from 'react';
+import Modal from 'react-modal'
+import LinkIcon from '@material-ui/icons/Link'
+import { ExpandMore} from '@material-ui/icons';
+import firebase from 'firebase';
 function Navbar() 
 {
   
@@ -33,7 +37,26 @@ function Navbar()
     
   } ,)
   const user = useSelector(selectUser)
+  const [openModal,setOpenModal] = useState(false)
   const logouthandler =() =>  auth.signOut()
+  const [input,setInput] = useState("")
+  const [inputUrl, setInputUrl] = useState("")
+  const questionName = input;
+  const handleQuestion =(e) => {
+    e.preventDefault()
+    
+    setOpenModal(false)
+    
+    db.collection('questions').add({
+    question: input,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    user : user,
+  })
+
+    setInput("")
+    setInputUrl("")
+  }
+  
   return (
     <div className = "qHeader">
        <div className = "qHeader_logo">
@@ -73,7 +96,70 @@ function Navbar()
          />
        </div>
        <LanguageIcon />
-       <Button>Add question</Button>
+       <Button onClick ={() => setOpenModal(true)}>Add question</Button>
+        <Modal
+         isOpen = {openModal}
+         onRequestClose = {() => setOpenModal(false)}
+         shouldCloseOnOverlayClick={false}
+         style ={{
+           overlay:
+           {
+           width:700,
+           height:600,
+           backgroundColor : "rgba(0,0,0,0.8)",
+           zIndex :"1000",
+           top: "50%",
+           left: "50%",
+           marginTop: "-300px",
+           marginLeft: "-350px",
+           },
+         }}
+        >
+           <div className = "modal__title">
+            <h5>Add Question</h5>
+            <h5>Share Link</h5>
+            </div>
+            <div className = "modal__info" >
+              <Avatar
+                className ="avatar"
+                src = {user.photo}
+              />
+              <p>{user.displayName ? user.displayName: user.email} asked </p>
+              <div className ="modal__scope">
+                <PeopleAltOutlinedIcon />
+                <p>Public</p>
+                <ExpandMore/>
+
+              </div>
+            </div>
+          <div className= "modal__Field">
+              <Input
+                required
+                value ={input}
+                onChange={(e)=> setInput(e.target.value)}
+                type ="text"
+                placeholder ="Start your question with 'What','How','Why' etc."
+              />
+
+            
+            <div className= "modal__fieldLink">
+              <LinkIcon />
+              <input
+                 value ={inputUrl}
+                 onChange={(e)=> setInputUrl(e.target.value)}
+                 type ="text"
+                placeholder ="Optional: include a link that gives context"
+              ></input>
+
+            </div>
+           </div>
+            <div className ="modal__buttons">
+              <button className="cancle">Cancel</button>
+             <button  onClick = {handleQuestion} type ="submit" className="add">
+              Add Question
+            </button>
+          </div> 
+       </Modal>
      </div>
     </div>
   )
